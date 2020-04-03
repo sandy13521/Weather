021839791b9ed35dbda0c0eb7a459e2f5fd3ee45
sandy13521 <nandavarmukund1999gmail.com>
2020-04-03 21:26:09 +0530

    Added Delete City Functionality and Rejecting Invalid Input.

--- WeatherApp/models.py
+++ WeatherApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class city(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
 
 
     def __str__(self):
--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -1,44 +1,34 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import city
-# from pyrebase import pyrebase
 from django.contrib import messages
-
-
-
-# firebaseConfig = {
-#     'apiKey': "AIzaSyCfLdUa2Gh65xTO_q1ESs30bWrlASdjAw0",
-#     'authDomain': "weather-247ba.firebaseapp.com",
-#     'databaseURL': "https://weather-247ba.firebaseio.com",
-#     'projectId': "weather-247ba",
-#     'storageBucket': "weather-247ba.appspot.com",
-#     'messagingSenderId': "312320080238",
-#     'appId': "1:312320080238:web:cc15e634870f55bbc6297f",
-#     'measurementId': "G-M23C5E9BWH"
-# }
-#
-# firebase = pyrebase.initialize_app(firebaseConfig)
-# auth = firebase.auth()
-
 
 # Getting City Weather Report By Using Api
 def city_weather(request):
+
     #Url with api ky and city
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='
-    if request.method == 'POST':
-        status = True
-        # TODO : If a city is already addd to the list don't add it again(UNIQUE) constraint will b voliated
-        cities = city.objects.all()
-        for i in cities:
-            print(i)
-            if i == request.POST['city']:
-                status = False
-                messages.info(request,'City Is Already Added')
-                break
-        if status:
-            city_name = city()
-            city_name.name = request.POST['city']
-            city_name.save()
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=bc94bea8e06b9bc36a7e9c46b6cda681'
+    if request.method == 'POST' :
+        city_requested = request.POST['city']
+        print(city.objects.filter(name = city_requested).count())
+        err_msg = ""
+        if city.objects.filter(name = city_requested).count() == 0 :
+            r = requests.get(url.format(city_requested))
+            report = r.json()
+            if report['cod'] == 200:
+                city_name = city()
+                city_name.name = city_requested
+                city_name.save()
+            else:
+                err_msg = "Invalid City Entered!!"
+                messages.error(request,err_msg)
+        else:
+            err_msg = "City Is Already Exists."
+            messages.error(request, err_msg)
+
+        if err_msg == "":
+            messages.success(request,"City is Succssfully Added.")
+
 
     weather_data = []
     cities = city.objects.all()
@@ -52,5 +42,13 @@ def city_weather(request):
             'Icon': report['weather'][0]['icon']
         }
         weather_data.append(City_Weather)
-    #print(weather_data)
     return render(request, 'report.html', {"weather_data": weather_data})
+
+
+# Method to Delete City From Database
+def delete(request,city_name):
+    try:
+        city.objects.get(name = city_name).delete()
+    finally:
+        return redirect('/city')
+
