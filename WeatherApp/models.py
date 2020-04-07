17d48897e48d8a61cdd8d2fa7e088729b6f4346c
sandy13521 <nandavarmukund1999gmail.com>
2020-04-07 20:09:01 +0530

    Updated Database Config, Refresh Option, Validation Message

--- WeatherApp/models.py
+++ WeatherApp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
 class city(models.Model):
-    name = models.CharField(max_length=100)
-
+    name = models.CharField(max_length=50)
+    time = models.DateTimeField()
+    temp = models.FloatField()
+    des = models.CharField(max_length=50, default="")
+    icon = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
